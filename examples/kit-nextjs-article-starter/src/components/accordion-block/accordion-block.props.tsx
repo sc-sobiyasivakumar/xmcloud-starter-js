db610c44d34fda16acb9e129976c9aa75254df10
sc-sobiyasivakumar <siva@sitecore.net>
2025-12-29 10:56:25 +0530

    Added lint error intentionally to check PR validation

--- examples/kit-nextjs-article-starter/src/components/accordion-block/accordion-block.props.tsx
+++ examples/kit-nextjs-article-starter/src/components/accordion-block/accordion-block.props.tsx
@@ -32,3 +32,6 @@ export type AccordionItemProps = {
     jsonValue?: RichTextField;
   };
 };
+
+// Intentional lint error for testing workflow - using 'any' type which is disallowed
+const testLintError: any = 'this should fail linting';
