460d8e05c7c4a677a455ec5731db216f57f2e8bd
sc-sobiyasivakumar <siva@sitecore.net>
2026-01-05 22:51:54 +0530

    unused import useMemo

--- examples/kit-nextjs-article-starter/src/hooks/use-client-width.tsx
+++ examples/kit-nextjs-article-starter/src/hooks/use-client-width.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState, type RefObject } from 'react';
+import { useEffect, useState, useMemo, type RefObject } from 'react';
 
 type WidthTransformFn = (width: number) => number;
 
