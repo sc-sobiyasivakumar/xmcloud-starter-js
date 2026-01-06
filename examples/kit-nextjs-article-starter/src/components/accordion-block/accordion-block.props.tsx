e248071a22d4679006d21b258243b6af0d6cb877
sc-sobiyasivakumar <siva@sitecore.net>
2026-01-06 16:21:10 +0530

    added lint in src/component

--- examples/kit-nextjs-article-starter/src/components/accordion-block/accordion-block.props.tsx
+++ examples/kit-nextjs-article-starter/src/components/accordion-block/accordion-block.props.tsx
@@ -1,5 +1,6 @@
 import type { Field, LinkField, RichTextField } from '@sitecore-content-sdk/nextjs';
 import type { ComponentProps } from '@/lib/component-props';
+import type React from 'react';
 
 /**
  * Model used for Sitecore Component integration
